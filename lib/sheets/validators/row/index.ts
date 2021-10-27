e11d41b00b7e1417284d8fd7dcf2bfbef60c57fa
Lucas <lucastfujiwara@gmail.com>
2021-10-27 09:46:34 -0300

    feat: add validation (socio)

--- lib/sheets/validators/row/index.ts
+++ lib/sheets/validators/row/index.ts
@@ -1,9 +1,11 @@
 import { AnoValidatorEntry } from "./ano.validator";
 import { CNAEValidatorEntry } from "./cnae.validator";
 import { CNPJValidatorEntry } from "./cnpj.validator";
+import { SocioValidatorEntries } from "./socio.validator";
 
 export const RowValidatorEntries = [
   CNPJValidatorEntry,
   CNAEValidatorEntry,
   AnoValidatorEntry,
+  ...SocioValidatorEntries,
 ];
