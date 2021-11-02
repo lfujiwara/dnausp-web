c52b02f54fdd78a8293363f7d9e42187a3791d67
dsnunes07 <dsnunes@usp.br>
2021-11-02 15:03:53 -0300

    add previous years revenue columns

--- lib/standardize/AvailableColumns.ts
+++ lib/standardize/AvailableColumns.ts
@@ -1,3 +1,9 @@
-const columns = ["Qual foi o faturamento da empresa em 2020? (R$)"];
+import { StandardizeRevenueColumn } from "../sheets/widgets/standardize-revenue-columns";
+
+const columns = [
+  "Qual foi o faturamento da empresa em 2020? (R$)",
+  "Faturamento 2018 (RFB)",
+  "Faturamento 2019 (ou estimativa da RFB) com porte",
+];
 
 export default columns;
