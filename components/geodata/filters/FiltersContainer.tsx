2b779219820ac7b922cca9eb408fac9dd86a8af2
dsnunes07 <dsnunes@usp.br>
2021-11-02 17:44:18 -0300

    rename folder

--- components/geodata/filters/FiltersContainer.tsx
+++
@@ -1,9 +0,0 @@
-import FilterSelector from "../filters/FilterSelector";
-
-export default function FiltersContainer() {
-  return (
-    <div className="filters-panel">
-      <FilterSelector />
-    </div>
-  );
-}
