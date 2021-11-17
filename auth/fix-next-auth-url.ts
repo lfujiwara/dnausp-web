7fe3498bd2928121a819e497a3253647f820699f
Lucas Fujiwara <lucastfujiwara@gmail.com>
2021-11-17 20:09:33 -0300

    fix: workaround next-auth url building bug (2)

--- auth/fix-next-auth-url.ts
+++ auth/fix-next-auth-url.ts
@@ -1,6 +1,6 @@
-const url = process.env.NEXTAUTH_URL + "";
-if (!url.startsWith("http")) {
-  process.env.NEXTAUTH_URL = "https://" + url;
+const url = process.env.VERCEL_URL + "";
+if (url && !url.startsWith("http")) {
+  process.env.NEXTAUTH_URL = "https://" + process.env.VERCEL_URL;
 }
 
 export {};
